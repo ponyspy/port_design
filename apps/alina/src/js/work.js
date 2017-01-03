c4970656c9183a73ed8306630e75ad80e75c60ef
ponyspy <desade4me@gmail.com>
2017-01-03 22:17:50 +0300

    styl

--- apps/alina/src/js/work.js
+++ apps/alina/src/js/work.js
@@ -5,6 +5,7 @@ $(function() {
 		var $gallery_thumbs = $(this).children('.gallery-thumbs');
 
 		var galleryTop = new Swiper($gallery_top, {
+			initialSlide: 2000,
 			nextButton: '.swiper-button-next',
 			prevButton: '.swiper-button-prev',
 			spaceBetween: 10,
@@ -13,6 +14,7 @@ $(function() {
 		var galleryThumbs = new Swiper($gallery_thumbs, {
 			spaceBetween: 10,
 			centeredSlides: true,
+			initialSlide: 2000,
 			slidesPerView: 'auto',
 			touchRatio: 0.2,
 			slideToClickedSlide: true
