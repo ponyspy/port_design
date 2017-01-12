132ba33b2be8a8418f0613bebebe8cbb3f2f7821
ponyspy <desade4me@gmail.com>
2017-01-12 20:41:15 +0300

    fix

--- apps/alina/src/js/main.js
+++ apps/alina/src/js/main.js
@@ -3,28 +3,32 @@ $(function() {
 	var deltaY = 0;
 	var step = 95;
 
-	$('.work_image').on('slidestep', function(e) {
-		var $this = $(this);
-		var $stack = $this.parent('.work_images').children('.work_image');
-
-		if ($stack.length > 1 && $this.index('.work_image') - 1 <= $stack.length) {
-			$stack.removeClass('active').filter(this).next().addClass('active');
-		} else {
-			$stack.removeClass('active').first().addClass('active');
-		}
+	$('.work_images').each(function() {
+		$(this).children('.work_image').first().addClass('active');
 	});
 
-	$('.work_image').on('mousemove', function(e) {
-		if (e.pageX >= deltaX || e.pageY >= deltaY) {
-			$(this).trigger('slidestep');
+	$('.work_image')
+		.on('slidestep', function(e) {
+			var $this = $(this);
+			var $stack = $this.parent('.work_images').children('.work_image');
 
-			deltaX = e.pageX + step;
-			deltaY = e.pageY + step;
-		} else if (e.pageX <= deltaX - step * 2 || e.pageY <= deltaY - step * 2) {
-			deltaX = e.pageX + step;
-			deltaY = e.pageY + step;
+			if ($stack.length > 1 && $this.index('.work_image') - 1 <= $stack.length) {
+				$stack.removeClass('active').filter(this).next().addClass('active');
+			} else {
+				$stack.removeClass('active').first().addClass('active');
+			}
+		})
+		.on('mousemove', function(e) {
+			if (e.pageX >= deltaX || e.pageY >= deltaY) {
+				$(this).trigger('slidestep');
 
-			$(this).trigger('slidestep');
-		}
-	});
+				deltaX = e.pageX + step;
+				deltaY = e.pageY + step;
+			} else if (e.pageX <= deltaX - step * 2 || e.pageY <= deltaY - step * 2) {
+				deltaX = e.pageX + step;
+				deltaY = e.pageY + step;
+
+				$(this).trigger('slidestep');
+			}
+		});
 });
